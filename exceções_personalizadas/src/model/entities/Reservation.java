8974c334db819d4c8cc638559e39681d4a3de151
jlacaTuga <jlaca@live.com.pt>
2020-02-10 15:05:10 -0300

    10/02/2020

---
+++ exceções_personalizadas/src/model/entities/Reservation.java
@@ -0,0 +1,72 @@
+package model.entities;
+
+import model.exceptions.DomainException;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+        if(!checkOut.after(checkIn)){
+            throw new DomainException("Check-out date must be after check-in date");
+        }
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        long diff=checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException {
+        Date now=new Date();
+        if(checkIn.before(now) || checkOut.before(now)){
+            throw new DomainException("Reservation dates for update must be future dates.");
+        }
+        if(!checkOut.after(checkIn)){
+            throw  new DomainException( "Checkout date must be after Checkin date.");
+        }
+
+        this.checkIn=checkIn;
+        this.checkOut=checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "roomNumber=" + roomNumber +
+                ", checkIn=" + checkIn +
+                ", checkOut=" + checkOut +
+                ", duration="+duration()+" dias"+
+                '}';
+    }
+
+
+}
